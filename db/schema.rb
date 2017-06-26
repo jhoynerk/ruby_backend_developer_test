86586d07c583107c27e7c5121583b487efc612eb
Jhoynerk Caraballo <jhoynerk@gmail.com>
2017-06-26 18:48:16 -0300

    add migrate appointments

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170327171217) do
+ActiveRecord::Schema.define(version: 20170626211015) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "appointments", force: :cascade do |t|
+    t.integer  "seller_id"
+    t.integer  "buyer_id"
+    t.datetime "date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["buyer_id"], name: "index_appointments_on_buyer_id", using: :btree
+    t.index ["seller_id"], name: "index_appointments_on_seller_id", using: :btree
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
@@ -23,4 +33,6 @@ ActiveRecord::Schema.define(version: 20170327171217) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "appointments", "users", column: "buyer_id"
+  add_foreign_key "appointments", "users", column: "seller_id"
 end
