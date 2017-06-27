bb05e5aa097bb65dd73f903cd505c08b70a6a522
Jhoynerk Caraballo <jhoynerk@gmail.com>
2017-06-27 08:37:08 -0300

    fix auth to generate token

--- app/lib/ruby_backend_developer_test/api/v1/auth.rb
+++ app/lib/ruby_backend_developer_test/api/v1/auth.rb
@@ -2,15 +2,22 @@ class RubyBackendDeveloperTest::API::V1::Auth < Grape::API
   version :v1, using: :path
 
   resources :auth do
+
+    params do
+      requires :credentials, type: Hash do
+        requires :email, :password
+      end
+    end
+
     post do
-      email, password = params[:credentials][:email], params[:credentials][:password]
+      email, password = params.credentials.email, params.credentials.password
       user = User.find_by_email(email)
       if user.nil?
         status :unauthorized
         { status: :unauthorized, message: 'User not found' }
       elsif user.valid_password?(password)
         status :ok
-        { status: :ok, token: Authentication::jwt(user) }
+        { status: :ok, token: Authentication::jwt_encode(user) }
       else
         status :unauthorized
         { status: :unauthorized, error: 'Password does not match' }
