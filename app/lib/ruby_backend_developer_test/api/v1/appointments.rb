6d3239518f1a5d8f125358b12770db58ea3f3385
Jhoynerk Caraballo <jhoynerk@gmail.com>
2017-06-27 08:37:25 -0300

    generate appointment

--- app/lib/ruby_backend_developer_test/api/v1/appointments.rb
+++ app/lib/ruby_backend_developer_test/api/v1/appointments.rb
@@ -2,6 +2,29 @@ class RubyBackendDeveloperTest::API::V1::Appointments < Grape::API
   version :v1, using: :path
 
   resources :appointments do
-    get { {pete: :rete} }
+    before { authenticate! }
+
+    params do
+      group :appointment, type: Hash do
+        requires :seller_id, :buyer_id, coerce: Integer
+        requires :date, coerce: DateTime
+      end
+    end
+
+    post do
+      attributes = params.fetch(:appointment)
+      appointment = Appointment.create({
+                                          seller_id: attributes.seller_id,
+                                          buyer_id: attributes.buyer_id,
+                                          date: attributes.date
+                                        })
+      if appointment.errors.any?
+        status :unprocessable_entity
+        { errors: appointment.errors.full_messages }
+      else
+        status :created
+        { data: appointment }
+      end
+    end
   end
 end
