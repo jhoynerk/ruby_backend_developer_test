05e3c81aff99f1948d7d1e23e264df3bc163ec07
Jhoynerk Caraballo <jhoynerk@gmail.com>
2017-06-27 08:36:25 -0300

    add relationship y validations.

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,2 +1,7 @@
 class Appointment < ApplicationRecord
+  belongs_to :seller, class_name: 'User'
+  belongs_to :buyer, class_name: 'User'
+
+  validates :seller, uniqueness: { scope: [ :seller, :buyer] }
+  validates :buyer, :seller, :date, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,10 @@
 class User < ApplicationRecord
 
+  has_many :appointments_as_seller, inverse_of: :seller
+  has_many :appointments_as_buyer, inverse_of: :buyer
+
+  validates :email, uniqueness: true
+
   # should be used devise or other gem to user auth
   def valid_password?(password)
     self.password == password
