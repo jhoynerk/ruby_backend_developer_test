c15935af7240559db8a6d55444f2939fc390285e
Jhoynerk Caraballo <jhoynerk@gmail.com>
2017-06-27 08:36:37 -0300

    fix authentication

--- app/models/authentication.rb
+++ app/models/authentication.rb
@@ -1,7 +1,11 @@
 class Authentication
   KEY_BASE = Rails.application.secrets.fetch(:secret_key_base)
 
-  def self.jwt(user)
-    JWT.encode({user_id: user.id}, Authentication::KEY_BASE)
+  def self.jwt_encode(user)
+    JWT.encode({ user_id: user.id }, Authentication::KEY_BASE)
+  end
+
+  def self.jwt_decode(token)
+    JWT.decode(token, Authentication::KEY_BASE)
   end
 end
