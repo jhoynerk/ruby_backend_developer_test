4b738cf0ca8c2828dec80ab7d5b9e09dc19f00b5
Jhoynerk Caraballo <jhoynerk@gmail.com>
2017-06-28 09:31:05 -0300

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170626211015) do
+ActiveRecord::Schema.define(version: 20170628120903) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20170626211015) do
     t.datetime "date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean  "status"
     t.index ["buyer_id"], name: "index_appointments_on_buyer_id", using: :btree
     t.index ["seller_id"], name: "index_appointments_on_seller_id", using: :btree
   end
